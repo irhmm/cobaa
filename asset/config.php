60ec2ab48e3090359ed89e5a5506657d24b8e831
irhmm <120614598+irhmm@users.noreply.github.com>
2023-11-19 22:23:17 +0700

    Add files via upload

--- asset/config.php
+++ asset/config.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 $server = "localhost";
 $user = "root";
@@ -9,4 +10,17 @@ if (!$conn) {
 }
 
 
+=======
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "p2md";
+$conn = mysqli_connect($server, $user, $pass, $database);
+if (!$conn) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+}
+
+
+>>>>>>> cd647118f0753bf2364a7fd6b4ff4a8157d315dd
 ?>
--- asset/login.php
+++ asset/login.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 include 'config.php';
 session_start();
@@ -125,4 +126,133 @@ if (isset($_POST['submit'])) {
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
+=======
+<?php
+include 'config.php';
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: asset/dash.php");
+    exit();
+}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username']; 
+    $password = hash('sha256', $_POST['password']); 
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: asset/dash.php");
+        exit();
+    } else {
+        echo "<script>alert('Username atau password Anda salah. Silakan coba lagi!')</script>";
+    }
+}
+?>
+
+
+<?php
+include 'config.php';
+
+if (isset($_SESSION['username'])) {
+    header("Location: asset/dash.php");
+    exit();
+}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username']; 
+    $password = $_POST['password']; 
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: asset/dash.php");
+        exit();
+    } else {
+        echo "<script>alert('Username atau password Anda salah. Silakan coba lagi!')</script>";
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>LOGIN | PENDATAAN RAMBU DISHUB KOTA MADIUN</title>
+    <link rel="icon" type="image/x-icon" href="img/dishublogo.png">
+    <style>
+        body {
+            background: #007bff;
+            background: linear-gradient(to right, #0062E6, #33AEFF);
+        }
+
+        .btn-login {
+            font-size: 0.9rem;
+            letter-spacing: 0.05rem;
+            padding: 0.75rem 1rem;
+        }
+
+        .btn-google {
+            color: white !important;
+            background-color: #ea4335;
+        }
+
+        .btn-facebook {
+            color: white !important; 
+            background-color: #3b5998;
+        }
+    </style>
+</head>
+<body>
+
+<section class="vh-100" style="background-color: #508bfc;">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card border-0 shadow rounded-3 my-5">
+                    <div class="card-body p-4 p-sm-5">
+                        <img src="../img/bedingin.png" class="rounded mx-auto d-block" width="130" height="150" alt="...">
+                        <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
+                        <form method="POST">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" name="username" placeholder="Username">
+                                <label for="floatingInput">Username</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+
+                            <div class="form-check mb-3">
+                                <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
+                                <label class="form-check-label" for="rememberPasswordCheck">
+                                    Remember password
+                                </label>
+                            </div>
+                            <div class="d-grid">
+                                <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" name="submit">Sign
+                                    in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+>>>>>>> cd647118f0753bf2364a7fd6b4ff4a8157d315dd
 </html>
